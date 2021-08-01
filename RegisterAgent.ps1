9f7f8b7dd4581e431488ec90f3fe6c3cfa1ed6fe
John Roth <johntroth@gmail.com>
2021-08-01 14:20:01 -0400

    Update RegisterAgent.ps1

--- RegisterAgent.ps1
+++ RegisterAgent.ps1
@@ -108,6 +108,7 @@ Remove-Item $agentZip;
 
 "Registering"
 # Register the agent in the environment
+"Calling .\config.cmd --unattended  --agent $agentName --runasservice --work '_work' --url $OrganizationUrl --auth PAT --token $Token --pool $Environment --replace --projectname $TeamProject --windowsLogonAccount $uid --windowsLogonPassword $pass"
 .\config.cmd --unattended  --agent $agentName --runasservice --work '_work' --url $OrganizationUrl --auth PAT --token $Token --pool $Environment --replace --projectname $TeamProject --windowsLogonAccount $uid --windowsLogonPassword $pass
 
 
