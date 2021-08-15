a6d43d66a5fe814b2519ef666ce9c0a686fc9520
John Roth <johntroth@gmail.com>
2021-08-15 12:17:34 -0400

    Update RegisterAgent.ps1

--- RegisterAgent.ps1
+++ RegisterAgent.ps1
@@ -41,10 +41,67 @@ If($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0")))
     throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell."
 };
 
+
+$agentName = $env:COMPUTERNAME;
+
 "Creating agent dir"
 If(-NOT (Test-Path $env:SystemDrive\'agent'))
 {
     mkdir $env:SystemDrive\'agent'
+    cd $env:SystemDrive\'agent';
+    $agentZip="$PWD\agent.zip";
+    "Donloading $agentZip"
+
+    # Configure the web client used to download the zip file with the agent
+    $DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;
+    $securityProtocol=@();
+    $securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;
+    $securityProtocol+=[Net.SecurityProtocolType]::Tls12;
+    [Net.ServicePointManager]::SecurityProtocol=$securityProtocol;
+    $WebClient=New-Object Net.WebClient; 
+    $WebClient.Headers.Add("user-agent", "azure pipeline");
+
+
+    ## Retrieve list with releases for the Azure Pipelines agent
+    $releasesUrl = "https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases"
+    if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($releasesUrl)))
+    {
+        $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($releasesUrl).OriginalString, $True);
+    };
+    $releases = $WebClient.DownloadString($releasesUrl) | ConvertFrom-Json
+
+
+    ## Select the newest agent release
+    $latestAgentRelease = $releases | Sort-Object -Property published_at -Descending | Select-Object -First 1
+    $assetsUrl = $latestAgentRelease.assets[0].browser_download_url
+
+    ## Get the agent download url from the agent release assets
+    if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($assetsUrl)))
+    {
+        $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($assetsUrl).OriginalString, $True);
+    };
+    $assets = $WebClient.DownloadString($assetsUrl) | ConvertFrom-Json
+    $Uri = $assets | Where-Object { $_.platform -eq "win-x64"} | Select-Object -First 1 | Select-Object -ExpandProperty downloadUrl
+
+    "Downloading from $Uri"
+
+    # Download the zip file with the agent
+    if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri)))
+    {
+        $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);
+    };
+    Write-Host "Download agent zip file from $Uri";
+    $WebClient.DownloadFile($Uri, $agentZip);
+
+    "Downloaded - Extracting $agentZip"
+    # Extract the zip file
+    Add-Type -AssemblyName System.IO.Compression.FileSystem;
+    [System.IO.Compression.ZipFile]::ExtractToDirectory( $agentZip, "$PWD");
+
+    "Removing zip"
+    # Remove the zip file
+    Remove-Item $agentZip;
+
 } else {
     cd $env:SystemDrive\'agent';
     "Removing previous agent"
@@ -52,69 +109,8 @@ If(-NOT (Test-Path $env:SystemDrive\'agent'))
     "Calling .\config.cmd remove --unattended --auth PAT --token $Token"
     .\config.cmd remove --unattended --auth PAT --token $Token
     cd $env:SystemDrive\
-    Start-Sleep -s 15
-    Remove-Item $env:SystemDrive\'agent' -force -recurse
-    mkdir $env:SystemDrive\'agent'
 }
 
-
-$agentName = $env:COMPUTERNAME;
-
-cd $env:SystemDrive\'agent';
-
-$agentZip="$PWD\agent.zip";
-
-"Donloading $agentZip"
-
-# Configure the web client used to download the zip file with the agent
-$DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;
-$securityProtocol=@();
-$securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;
-$securityProtocol+=[Net.SecurityProtocolType]::Tls12;
-[Net.ServicePointManager]::SecurityProtocol=$securityProtocol;
-$WebClient=New-Object Net.WebClient; 
-$WebClient.Headers.Add("user-agent", "azure pipeline");
-
-
-## Retrieve list with releases for the Azure Pipelines agent
-$releasesUrl = "https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases"
-if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($releasesUrl)))
-{
-    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($releasesUrl).OriginalString, $True);
-};
-$releases = $WebClient.DownloadString($releasesUrl) | ConvertFrom-Json
-
-
-## Select the newest agent release
-$latestAgentRelease = $releases | Sort-Object -Property published_at -Descending | Select-Object -First 1
-$assetsUrl = $latestAgentRelease.assets[0].browser_download_url
-
-## Get the agent download url from the agent release assets
-if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($assetsUrl)))
-{
-    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($assetsUrl).OriginalString, $True);
-};
-$assets = $WebClient.DownloadString($assetsUrl) | ConvertFrom-Json
-$Uri = $assets | Where-Object { $_.platform -eq "win-x64"} | Select-Object -First 1 | Select-Object -ExpandProperty downloadUrl
-
-"Downloading from $Uri"
-
-# Download the zip file with the agent
-if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri)))
-{
-    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);
-};
-Write-Host "Download agent zip file from $Uri";
-$WebClient.DownloadFile($Uri, $agentZip);
-
-"Downloaded - Extracting $agentZip"
-# Extract the zip file
-Add-Type -AssemblyName System.IO.Compression.FileSystem;
-[System.IO.Compression.ZipFile]::ExtractToDirectory( $agentZip, "$PWD");
-
-"Removing zip"
-# Remove the zip file
-Remove-Item $agentZip;
 
 "Registering"
 # Register the agent in the environment
